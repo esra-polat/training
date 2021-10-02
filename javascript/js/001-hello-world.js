5a6c904d862b0694b90c36b7803bb606793e019a
esra-polat <esrapolat1293@gmail.com>
2021-10-02 16:09:02 +0300

    js

--- javascript/js/001-hello-world.js
+++ javascript/js/001-hello-world.js
@@ -1,10 +1,10 @@
 // ********** Merhaba Dunya Ornekleri ********** 
 
 // Dokumana(document) Yazdirma Islemi ile Merhaba Dunya:
-// document.write('Merhaba Dunya');
+document.write('hello world!');
 
 // Uyari Mesaji (Alert) ile Merhaba Dunya:
-// alert("Merhaba Dunya")
+alert("Hey!")
 
 // console.log ile Merhaba Dunya:
 console.log("Merhaba Dunya")
