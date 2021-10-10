e2ccccd47534f7c585b48b984d36667043c37748
esra-polat <esrapolat1293@gmail.com>
2021-10-11 01:11:01 +0300

    array in js

--- javascript/js/032-cok-kullanilan-array-dizi-metotlari.js
+++ javascript/js/032-cok-kullanilan-array-dizi-metotlari.js
@@ -59,3 +59,174 @@ console.log(allUsers.join(" --- "))
 allUsers.splice(allUsers.length -1, 0, "Melissa")
 allUsers.splice(Math.floor( allUsers.length / 2 ), 0, "Lorem")
 console.log(allUsers)
+
+// -----------------------------------
+
+const alisverisListem = ["elma", "ekmek", "süt"];
+
+const stringAlisverisListem =  alisverisListem.join();
+console.log(stringAlisverisListem);
+// Çıktıda "elma,ekmek,süt" bekleriz.
+
+const stringAlisverisListem2 = alisverisListem.join(" kiraz ");
+console.log(stringAlisverisListem2);
+// Çıktıda "elma kiraz ekmek kiraz süt" bekleriz.
+
+const yiyecekler = ["pasta", "baklava", "puding"];
+const icecekler = ["su", "kahve"];
+
+const menu = yiyecekler.concat(icecekler);
+console.log(menu);
+// Çıktıda ["pasta", "baklava", "puding", "su", "kahve"] bekleriz.
+
+// --------------------------
+
+// dizi.forEach(function(diziElemanınınKendisi, diziElemanınınIndeksi, dizininKendisi) {
+//     // Bir şeyler yap.
+//     }); 
+
+// Malzemeler dizisindeki her bir malzemeyi ve onun indeksini konsola yazdıran bir kod yazalım:
+
+const malzemeler = ["yumurta", "un", "süt"];
+
+malzemeler.forEach(function(malzeme, malzemeIndeksi) {
+    console.log(malzeme, malzemeIndeksi);
+});
+
+// console.log() fonksiyonu her bir malzeme için ayrı ayrı çalışacağından çıktı olarak tüm malzemeleri (ve indekslerini) alt alta görürüz.
+
+// Bir dizideki sayıların 5 katından oluşan başka bir dizi oluşturalım:
+
+const sayilar = [1,2,3];
+
+let sayilarin5kati = sayilar.map(function(sayi) {
+    return sayi*5;
+});
+
+console.log(sayilarin5kati);
+// Çıktı olarak [5,10,15] görmeyi bekleriz.
+
+console.log(sayilar);
+// Çıktı olarak [1,2,3] görmeyi bekleriz. Orijinal dizimiz aynı kalır.
+
+
+const sayilar = [4,5,6];
+
+// sonucu görebilmek çıktıyı sonuc adlı değişkende tutalım:
+const sonuc1 = sayilar.some(function(sayi) {
+    return sayi > 5 ;
+});
+
+console.log(sonuc1);
+// Dizi içerisinde 5'ten büyük bir eleman bulunduğu için çıktıda "true" görmeyi bekleriz.
+
+
+const sonuc2 = sayilar.some(function(sayi) {
+    return sayi <3;
+});
+
+console.log(sonuc2);
+// Dizi içerisinde 3'ten küçük herhangi bir sayı olmadığı için çıktıda "false" görmeyi bekleriz.
+
+const sayilar = [4,5,6];
+
+const sonuc1 = sayilar.every(function(sayi) {
+    return sayi > 3;
+});
+
+console.log(sonuc1);
+// Dizideki tüm sayılar 3'ten büyük olduğu için bu sonucun true dönmesini bekleriz.
+
+
+const sonuc2 = sayilar.every(function(sayi) {
+    return sayi > 5;
+});
+
+console.log(sonuc2);
+// Dizideki tüm sayılar 5'ten büyük olmadığı için sonucun false dönmesini bekleriz.
+
+
+const sonuc3 = sayilar.every(function(sayi) {
+    return sayi > 7;
+});
+
+console.log(sonuc3);
+// Dizideki hiçbir sayı 7'den büyük olmadığı için sonucun false dömmesini bekleriz.
+
+const sayilar = [1,2,3,4,5];
+
+const filtrelenmisSayilar = sayilar.filter(function(sayi) {
+    return sayi >3;
+});
+
+console.log(filtrelenmisSayilar);
+// Orijinal diziyi 3'ten büyük olan sayılar için filtrelediğimizde yeni oluşacak dizi [4,5] olacaktır.
+
+console.log(sayilar);
+// Orijinal dizi bozulmayacağından çıktıda [1,2,3,4,5] olarak görürüz.
+
+// ---------------------
+
+const sayilar = [4,5,6,7];
+
+const bulunacakEleman1 = sayilar.find(function(sayi) {
+    return sayi === 5;
+});
+console.log(bulunacakEleman1);
+// Dizi içerisinde 5 eleman olarak bulunduğu için çıktıda elemanın kendisini yani 5 görmeyi bekleriz.
+
+
+const bulunacakEleman2 = sayilar.find(function(sayi) {
+    return sayi > 5;
+});
+console.log(bulunacakEleman2);
+// Dizi içerisinde 5'ten büyük birden fazla eleman olduğu için koşula uyan ilk elemanı yani 6'yı görmeyi bekleriz.
+
+
+const bulunacakEleman3 = sayilar.find(function(sayi) {
+    return sayi < 3;
+});
+console.log(bulunacakEleman3);
+// Dizi içerisinde 3'ten küçük bir eleman olmadığı için çıktıda undefined görmeyi bekleriz.
+
+
+// -------------------------------
+
+const sayilar = [3,5,2,10,4];
+
+sayilar.sort();
+console.log(sayilar);
+// Çıktı olarak [10, 2, 3, 4, 5] alırız. 
+//(Sayıların string halleri UTF-16 değerlerine göre sıralandıkları için)
+
+const sayilar = [3,5,2,10,4];
+
+sayilar.sort(function(a,b) {
+    return b-a;
+});
+console.log(sayilar);
+// Çıktı olarak [10, 5, 4, 3, 2] alırız.
+
+// -------------------------
+
+const sayilar = [10,20,30];
+
+// Dizi içindeki sayıları toplayarak indirgeyecek bir fonksiyon yazalım:
+function indirgeyici (akumulator, sayi) {
+    return akumulator + sayi;
+}
+
+// Bu fonksiyonu ve toplamaya 0'dan başlayacağımızı belirten 0 sayısını metodumuza parametre olarak girelim ve sonucu bir değişkende tutalım:
+const sonuc1 = sayilar.reduce(indirgeyici,0);
+
+console.log(sonuc1);
+//0 + 10 + 20 + 30 = 60 olacağından çıktı olarak 60 bekleriz.
+
+
+
+// Eğer akümülatorümüzü 0 yerine 5'den başlatsaydık çıkabilecek sonucu görelim:
+
+const sonuc2 = sayilar.reduce(indirgeyici, 5);
+
+console.log(sonuc2);
+// 5 + 10 + 20 + 30 = 65 olacağından çıktı olarak 65 bekleriz.
