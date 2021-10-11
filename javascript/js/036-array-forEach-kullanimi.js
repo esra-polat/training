35231d8e6f4d6358c43142d4e06fe7921eda17fa
esra-polat <esrapolat1293@gmail.com>
2021-10-11 16:10:22 +0300

    array foeach

--- javascript/js/036-array-forEach-kullanimi.js
+++ javascript/js/036-array-forEach-kullanimi.js
@@ -26,3 +26,45 @@ PRODUCTS.forEach( item => {
   liDOM.innerHTML = item
   userListDOM.append(liDOM)
 })
+
+
+// JavaScript'de forEach Nasıl Kullanılır?
+// JavaScript'de forEach , döngü oluşturmamızı ve bu döngüyü sırayla çalıştırmamızı sağlayan bir array metodudur.
+
+// forEach fonksiyonu item, index, array olmak üzere 3 parametre alabilir.
+
+// arr.forEach(function(value, index, array) {
+//   // index ve array kullanmak opsiyoneldir
+// }
+// Konuyu daha iyi anlamak için her parametrenin aldığı değeri gösteren bir örnek yapalım.
+
+// const animals = ["cat" , "dog" , "bird", "horse"];
+  
+// animals.forEach((value , index , array) => {
+//   console.log('value: ', value );
+//   console.log('value parametresinin aldığı index :', index );
+//   console.log('array:' , array );
+// });
+// Aynı fonksiyonu her zaman kullanmak zorunda olduğumuz index parametresiyle yazalım.
+
+// const animals = ["cat" , "dog" , "bird", "horse"];
+// animals.forEach( animal => console.log( animal ) );
+// //Arrow function gosterimi(ES6)
+// Şimdi de forEach kullanarak yeni bir array oluşturabileceğimiz bir fonksiyon yazalım.
+
+// const numbers = [4, 11, 9];
+// const newArray = [];
+
+// numbers.forEach(function(numbers){
+//   newArray.push(numbers*3);
+// });
+// console.log(newArray);
+
+// // output = [12, 33, 27]
+// Numbers array'ini kullanarak her elemanının iki fazlasına sahip olan başka bir array oluşturunuz.
+
+//  const numbers = [12, 24, 36]; 
+
+//  numbers.forEach(() => {
+//      //...
+//  })
